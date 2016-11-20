a5cfd1c109025c1147c973489dcbb0ebc8b0f12e
Stuart Hoffman <stuhoffman@users.noreply.github.com>
2016-11-20 11:52:55 -0800

    Saving the progress nothing is really broken or working at this time

--- StuHoffmanResume/ViewController.swift
+++ StuHoffmanResume/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   var myCounter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func pressMePressed(_ sender: UIButton) {
+        myCounter = myCounter + 1
+        
+        print("Counting up to \(myCounter)")
+        for x in 0..<myCounter {
+            print("x = \(x)")
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
